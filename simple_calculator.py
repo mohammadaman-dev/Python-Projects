f95c108c82e8b1e109788796f79f745ccf82ed51
mohammadaman-dev <amanmohammad0999@gmail.com>
2026-06-29 16:45:04 +0530

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,14 @@
+print("=== SIMPLE CALCULATOR ===")
+
+num1 = 15
+num2 = 5
+
+print(f"\nfirst number: {num1}")
+print(f"second number: {num2}")
+
+print("\n=== Results ===")
+
+print("Addition         =",  num1 + num2)
+print("Subtraction      =", num1 - num2)
+print("Multiplication   =", num1 * num2)
+print("Division         =", num1 / num2)
